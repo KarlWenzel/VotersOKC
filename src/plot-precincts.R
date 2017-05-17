bd147a5f990bc0a30037c81809818760e5066fb6
Karl <Karl>
2017-05-17 00:02:28 -0500

    Precincts in Color

--- src/plot-precincts.R
+++ src/plot-precincts.R
@@ -1,5 +1,14 @@
 require(rgdal)
+library("RColorBrewer")
 
 projectPath = "E:/git/VotersOKC"
 data = readOGR(paste(projectPath, "/shapefiles", sep=""), "OKC_Joined")
-plot(data)
+
+df = data.frame(seq = 1:nrow(data), precinct = as.numeric(as.character(data$Precinct)))
+df = df[order(df$precinct),]
+df$colSeq = 1:nrow(data)
+df = df[order(df$seq),]
+
+cols = colorRampPalette(brewer.pal(length(data),"Spectral"))(length(data))
+cols = cols[df$colSeq]
+plot(data, col=cols)
