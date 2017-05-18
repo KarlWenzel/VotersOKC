625f09330b7d8ca0ea25fcff8f519c8397154282
Karl <Karl>
2017-05-18 00:00:14 -0500

    labeling the precincts on the map plot

--- src/plot-precincts.R
+++ src/plot-precincts.R
@@ -1,14 +1,26 @@
-require(rgdal)
+# NOTE:  gCentroid is from the 'rgeos' package
+library(rgeos)
+library(rgdal)
 library("RColorBrewer")
 
 projectPath = "E:/git/VotersOKC"
 data = readOGR(paste(projectPath, "/shapefiles", sep=""), "OKC_Joined")
+data$Precinct = as.numeric(as.character(data$Precinct))
 
-df = data.frame(seq = 1:nrow(data), precinct = as.numeric(as.character(data$Precinct)))
+df = data.frame(seq = 1:nrow(data), precinct = data$Precinct)
 df = df[order(df$precinct),]
 df$colSeq = 1:nrow(data)
 df = df[order(df$seq),]
 
-cols = colorRampPalette(brewer.pal(length(data),"Spectral"))(length(data))
+cols = colorRampPalette(brewer.pal(11,"Spectral"))(length(data))
 cols = cols[df$colSeq]
-plot(data, col=cols)
+
+# Find the center of each region and label lat and lon of centers
+centroids <- gCentroid(data, byid=TRUE)
+centroidLongs <- coordinates(centroids)[,1]
+centroidLats <- coordinates(centroids)[,2]
+
+plot(data, col=cols, main="OKC Precincts")
+text(centroidLongs, centroidLats, labels=df$precinct, col="black", cex=.7)
+
+
